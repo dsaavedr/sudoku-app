b5027ad2f619d8d6f818c7681c3dae683cba1fab
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-02-25 02:45:48 -0500

    Changed navigator options to fix white flash when changing route and added game view

--- App.js
+++ App.js
@@ -1,35 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import { DarkTheme, NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { Home, About } from './src/views';
-import {MAIN_BACKGROUND_COLOR} from './src/constants';
+import { Home, About, Sudoku } from './src/views';
+import { LIGHT_BACKGROUND_COLOR, MAIN_BACKGROUND_COLOR } from './src/constants';
 
 const Stack = createStackNavigator();
 
+const navigatorOptions = {
+  cardStyle: {backgroundColor: MAIN_BACKGROUND_COLOR},
+  cardStyleInterpolator: ({ current: { progress } }) => ({
+    cardStyle: {
+      opacity: progress.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 1]
+      })
+    },
+    overlayStyle: {
+      opacity: progress.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 0.5],
+        extrapolate: 'clamp'
+      })
+    }
+  }),
+  presentation: "modal",
+  headerStyle: {
+    backgroundColor: LIGHT_BACKGROUND_COLOR
+  },
+  headerTintColor: "#fff"
+};
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerShown: false
-          }}
-      />
-        <Stack.Screen
-          name="About"
-          component={About}
-        />
-      </Stack.Navigator>
+    <NavigationContainer theme={DarkTheme}>
+        <Stack.Navigator
+          screenOptions={navigatorOptions}
+        >
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen
+            name="Sudoku"
+            component={Sudoku}
+          />
+          <Stack.Screen
+            name="About"
+            component={About}
+          />
+        </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  
+
 });
 
