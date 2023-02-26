a491964737414d4a98f7f9887779b84bd507f9dd
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-02-26 00:38:54 -0500

    Added new component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from "./Button";
 import Footer from './Footer';
 import Board from './Board';
 import Box from "./Box";
+import ButtonList from "./ButtonList";
 
-export {Button, Footer, Board, Box};
+export {Button, Footer, Board, Box, ButtonList};
