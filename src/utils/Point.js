c3f438f637a82b3aae5e08b3736426dfc988d253
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-03-13 00:03:54 -0500

    Added class methods for obtaining all unique neighbors

--- src/utils/Point.js
+++ src/utils/Point.js
@@ -1,9 +1,13 @@
+import { arraysEqual } from ".";
+
 export default class Point {
   constructor({ x = 0, y = 0, value = null, fixed = false }) {
     this.x = x;
     this.y = y;
     this.value_ = value;
     this.fixed = fixed;
+    this.neighbors = [];
+    this.generateNeighbors();
   }
 
   set value(value) {
@@ -14,5 +18,41 @@ export default class Point {
 
   get value() {
     return this.value_;
+  }
+
+  generateNeighbors() {
+    // Row and column
+    for (let i = 0; i < 9; i++) {
+      this.neighbors.push([i, this.y]);
+      this.neighbors.push([this.x, i]);
+    }
+
+    // Block
+    const xb = Math.floor(this.x / 3) * 3;
+    const yb = Math.floor(this.y / 3) * 3;
+
+    for (let i = xb; i < xb + 3; i++) {
+      for (let j = yb; j < yb + 3; j++) {
+        this.neighbors.push([i, j]);
+      }
+    }
+    this.uniqueNeighbors();
+  }
+
+  uniqueNeighbors() {
+    const uniqueNeighbors = [];
+    for (const el of this.neighbors) {
+      let unique = true;
+      for (const uel of uniqueNeighbors) {
+        if (arraysEqual(el, uel)) {
+          unique = false;
+          break;
+        }
+      }
+      if (unique) {
+        uniqueNeighbors.push(el);
+      }
+    }
+    this.neighbors = uniqueNeighbors;
   }
 }
