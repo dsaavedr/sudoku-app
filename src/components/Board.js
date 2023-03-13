25df01b0271f0c810c5696b4a1625e84b0a55470
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-03-13 00:04:17 -0500

    Refactor for order

--- src/components/Board.js
+++ src/components/Board.js
@@ -20,14 +20,12 @@ export default function Board() {
     const cellPoints = [];
     for (let x = 0; x < 9; x++) {
       for (let y = 0; y < 9; y++) {
-        cellPoints.push(
-          new Point({
-            x,
-            y,
-            value:
-              Math.random() > 0.7 ? 1 + Math.floor(Math.random() * 9) : null,
-          })
-        );
+        const point = new Point({
+          x,
+          y,
+          value: Math.random() > 0.7 ? 1 + Math.floor(Math.random() * 9) : null,
+        });
+        cellPoints.push(point);
       }
     }
     dispatch(setCells(cellPoints));
