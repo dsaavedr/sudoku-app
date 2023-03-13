ede23fa33c0969debc545b1f60d5c69f9030f9e2
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-03-13 00:03:32 -0500

    Added method for comparing arrays

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,16 @@ import Point from "./Point";
 
 export const IX = (x, y, w) => y * w + x;
 
+export const arraysEqual = (a, b) => {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+
+  return true;
+};
+
 export { Point };
