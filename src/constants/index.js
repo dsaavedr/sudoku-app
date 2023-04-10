87567ee52d6e515b26a890595a54a0823ef69cb1
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-04-10 00:55:17 -0500

    Added new constants: color and difficulty levels

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,12 @@
 export const PRIMARY_COLOR = "#852064";
 export const SECONDARY_COLOR = "#e9c253";
+export const TERTIARY_COLOR = "#3273fa";
 export const MAIN_BACKGROUND_COLOR = "#25292e";
 export const LIGHT_BACKGROUND_COLOR = "#434b54";
 export const BOARD_WIDTH = 330;
+export const DIFFICULTY = {
+  EASY: "easy",
+  MEDIUM: "medium",
+  HARD: "hard",
+  EXTREME: "extreme",
+};
