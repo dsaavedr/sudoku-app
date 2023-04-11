f73565aeb450121bb81f752cccd82ddfcd8dfd32
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-04-11 18:16:38 -0500

    Added styles based on index as a workaround for no border collapse

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,37 @@
 import { StyleSheet, Text, View } from "react-native";
-import { SECONDARY_COLOR } from "../constants";
+import { BOARD_WIDTH, SECONDARY_COLOR, TERTIARY_COLOR } from "../constants";
 
 export default function Cell({ point }) {
+  const { x, y } = point;
+  const cellStyles = { ...styles.cell };
+  // Block styles
+  if (y % 3 === 0) {
+    cellStyles.borderTopColor = "#bbb";
+    cellStyles.borderTopWidth = 2;
+  }
+  if (x % 3 === 0) {
+    cellStyles.borderLeftColor = "#bbb";
+    cellStyles.borderLeftWidth = 2;
+  }
+  if (x === 8) {
+    cellStyles.borderRightColor = "#bbb";
+    cellStyles.borderRightWidth = 2;
+  }
+  if (y === 8) {
+    cellStyles.borderBottomColor = "#bbb";
+    cellStyles.borderBottomWidth = 2;
+  }
+  // Cell styles
+  if (x % 3 === 0 || x % 3 === 1) {
+    cellStyles.borderRightColor = "#888";
+    cellStyles.borderRightWidth = 1;
+  }
+  if (y % 3 === 0 || y % 3 === 1) {
+    cellStyles.borderBottomColor = "#888";
+    cellStyles.borderBottomWidth = 1;
+  }
   return (
-    <View style={styles.cell}>
+    <View style={cellStyles}>
       <Text style={styles.cellValue}>{point.value}</Text>
     </View>
   );
@@ -11,12 +39,10 @@ export default function Cell({ point }) {
 
 const styles = StyleSheet.create({
   cell: {
-    borderColor: SECONDARY_COLOR,
     alignItems: "center",
     justifyContent: "center",
-    borderWidth: 1,
-    width: `${(1 / 9) * 100}%`,
-    height: `${(1 / 9) * 100}%`,
+    width: Math.floor(BOARD_WIDTH / 9),
+    height: Math.floor(BOARD_WIDTH / 9),
   },
   cellValue: {
     color: "#fff",
