3f155b51306a12c2696abb00a384af1d134c5196
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-04-11 18:17:03 -0500

    Changed width so it's divisible by 9

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ export const SECONDARY_COLOR = "#e9c253";
 export const TERTIARY_COLOR = "#3273fa";
 export const MAIN_BACKGROUND_COLOR = "#25292e";
 export const LIGHT_BACKGROUND_COLOR = "#434b54";
-export const BOARD_WIDTH = 330;
+export const BOARD_WIDTH = 333;
 export const DIFFICULTY = {
   EASY: "easy",
   MEDIUM: "medium",
