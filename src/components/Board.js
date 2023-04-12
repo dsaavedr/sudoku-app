48d695f5f6fa8a39ba785ad14efd86798bf9ade1
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-04-11 21:31:52 -0500

    Introduced logic for passing flags to selected cells for conditional formatting

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,28 +1,65 @@
+import { useEffect, useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { BOARD_WIDTH } from "../constants";
+import { selectCell } from "../state/boardSlice";
+import { IX } from "../utils";
 import Cell from "./Cell";
 
 export default function Board() {
-    const cells = useSelector(state => state.board.cells);
+  const dispatch = useDispatch();
+  const cells = useSelector((state) => state.board.cells);
+  const selectedCell = useSelector((state) => state.board.selectedCell);
+  const [selectedNeighbors, setSelectedNeighbors] = useState([]);
 
-    return (
-        <View style={styles.container}>
-            <FlatList
-                data={cells}
-                renderItem={({ item, index }) => {
-                    return <Cell point={item} key={`cell-${index}`} />;
-                }}
-                numColumns={9}
-            />
-        </View>
+  useEffect(() => {
+    if (!selectedCell || !cells[selectedCell]) return;
+    setSelectedNeighbors(
+      cells[selectedCell].neighbors.map((arr) => IX(arr[0], arr[1], 9))
     );
+    console.log(selectedNeighbors);
+  }, [selectedCell]);
+
+  const onSelectCell = (cell) => {
+    dispatch(
+      selectCell({
+        x: cell.x,
+        y: cell.y,
+      })
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={cells}
+        renderItem={({ item, index }) => {
+          let selected = false;
+          if (index === selectedCell) {
+            selected = true;
+          }
+          return (
+            <Cell
+              onPress={() => {
+                onSelectCell(item);
+              }}
+              selected={selected}
+              neighbor={selectedNeighbors.includes(index)}
+              point={item}
+              key={`cell-${index}`}
+            />
+          );
+        }}
+        numColumns={9}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: BOARD_WIDTH,
-        height: BOARD_WIDTH
-    }
+  container: {
+    width: BOARD_WIDTH,
+    height: BOARD_WIDTH,
+  },
 });
