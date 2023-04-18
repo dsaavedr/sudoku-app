ee4162d5a63149b6e288967c3736d37725e42eb4
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-04-18 11:42:19 -0500

    Made lighter

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const PRIMARY_COLOR = "#852064";
 export const SECONDARY_COLOR = "#e9c253";
-export const SECONDARY_COLOR_LIGHT = "#f0d68c";
+export const SECONDARY_COLOR_LIGHT = "#f3d98f";
 export const TERTIARY_COLOR = "#3273fa";
 export const MAIN_BACKGROUND_COLOR = "#25292e";
 export const LIGHT_BACKGROUND_COLOR = "#434b54";
