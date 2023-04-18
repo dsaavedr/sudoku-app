666887298540799ebf17c02c9f696ea59e543bc0
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-04-18 11:43:18 -0500

    Added new view

--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,6 @@
 import Home from "./Home";
 import About from "./About";
 import Sudoku from "./Sudoku";
+import GameEnded from "./GameEnded";
 
-export { Home, About, Sudoku };
+export { Home, About, Sudoku, GameEnded };
