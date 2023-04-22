eae948736dbc455e553714833bc2c462a2112fc7
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-04-21 22:12:31 -0500

    Added store persistor

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import { DarkTheme, NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Provider } from "react-redux";
 import * as SplashScreen from "expo-splash-screen";
+import { PersistGate } from "redux-persist/integration/react";
 
 import { Home, About, Sudoku, GameEnded, DifficultySelect } from "./src/views";
 import { LIGHT_BACKGROUND_COLOR, MAIN_BACKGROUND_COLOR } from "./src/constants";
-import store from "./src/state/store";
+import store, { persistor } from "./src/state/store";
 import { useCallback, useEffect, useState } from "react";
 
 const Stack = createStackNavigator();
@@ -67,34 +68,36 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <NavigationContainer theme={DarkTheme} onReady={onLayoutRootView}>
-        <Stack.Navigator screenOptions={navigatorOptions}>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen name="Sudoku" component={Sudoku} />
-          <Stack.Screen name="About" component={About} />
-          <Stack.Screen
-            name="Difficulty"
-            component={DifficultySelect}
-            options={{
-              headerTitle: "New Game",
-            }}
-          />
-          <Stack.Screen
-            name="Game Ended"
-            component={GameEnded}
-            options={{
-              headerShown: false,
-            }}
-          />
-        </Stack.Navigator>
-        <StatusBar style="auto" />
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer theme={DarkTheme} onReady={onLayoutRootView}>
+          <Stack.Navigator screenOptions={navigatorOptions}>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen name="Sudoku" component={Sudoku} />
+            <Stack.Screen name="About" component={About} />
+            <Stack.Screen
+              name="Difficulty"
+              component={DifficultySelect}
+              options={{
+                headerTitle: "New Game",
+              }}
+            />
+            <Stack.Screen
+              name="Game Ended"
+              component={GameEnded}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+          <StatusBar style="auto" />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- src/state/store.js
+++ src/state/store.js
@@ -1,15 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "@react-native-async-storage/async-storage";
+import { persistReducer, persistStore } from "redux-persist";
 
 import boardReducer from "./boardSlice";
 import loggerMiddleware from "../middleware/logger";
 import validationMiddleware from "../middleware/validate";
 
-export default configureStore({
-  reducer: {
-    board: boardReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  board: boardReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }).concat(loggerMiddleware, validationMiddleware),
 });
+
+export const persistor = persistStore(store);
+export default store;
